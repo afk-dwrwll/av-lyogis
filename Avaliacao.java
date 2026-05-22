47eccb7ed4f483193b700977d21a38a7d8dd99f3
afk-dwrwll <202611190044@ifba.edu.br>
2026-05-22 00:05:31 +0000

    Atualiza Avaliacao.java

--- Avaliacao.java
+++ Avaliacao.java
@@ -3,5 +3,28 @@
     Boa prova! */
 
 void main() {
-    IO.println("Boa Prova!");
+    double consumoDoDia;
+    double consumoTotal = 0;
+    int totalDias = 0;
+    double media;
+    consumoDoDia = Double.parseDouble(IO.readln("Consumo total no dia foi de:"));
+
+    while (consumoDoDia != 0) {  
+        consumoTotal = consumoTotal + consumoDoDia;
+     totalDias++;
+     consumoDoDia = Double.parseDouble(IO.readln("Consumo do outro dia: "));
+    }
+    if (totalDias > 0) {
+        media = consumoTotal / totalDias;
+      System.out.printf("A media de consumo foi: %.2f litros\n", media);
+        if (media < 100) {
+      System.out.println("Consumo Sustentável");
+        } else if (media <= 175) {
+      System.out.println("Consumo Moderado");
+        } else {
+        System.out.println("Consumo Excessivo");
+        }
+    } else {
+        System.out.println("Precisa ter consumo");
+    }
 }
